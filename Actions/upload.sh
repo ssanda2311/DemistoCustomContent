8b00e580f07089dde12a52d5d315ce2f6c0c4cc6
ssanda2311 <sharma.guru25@gmail.com>
2025-12-28 21:59:29 +0530

    Update upload.sh

--- Actions/upload.sh
+++ Actions/upload.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-PACK_NAME="$1"
+PACK_PATH="$1"
 
-if [ -z "$PACK_NAME" ]; then
+if [ -z "$PACK_PATH" ]; then
   echo "Usage: $0 <PackName>"
   exit 1
 fi
 
-PACK_PATH="Packs/${PACK_NAME}"
-
-cd ..
 python3 demisto_sdk/__main__.py upload -i "${PACK_PATH}" --xsiam
