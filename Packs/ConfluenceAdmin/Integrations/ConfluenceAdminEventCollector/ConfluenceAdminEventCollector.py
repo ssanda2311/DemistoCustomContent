ec20cea676b173a863238ef4887c801ebf571179
ssanda2311 <sharma.guru25@gmail.com>
2026-01-26 19:55:20 +0530

    Update integration comments

--- Packs/ConfluenceAdmin/Integrations/ConfluenceAdminEventCollector/ConfluenceAdminEventCollector.py
+++ Packs/ConfluenceAdmin/Integrations/ConfluenceAdminEventCollector/ConfluenceAdminEventCollector.py
@@ -116,6 +116,20 @@ def deduplicate_events(data: list[dict], event_ids: list[str], fetch_time: str) 
     return updated_data
 
 
+def should_refetch_events(events: list[dict[str,str]], last_event_ids: list):
+    '''
+    Function to check if events should be refetched with reduced timestamp
+    '''
+    if not events:
+        return False
+    if not last_event_ids:
+        return False
+    if overlap_exists(events, last_event_ids):
+        return False
+    
+    return True
+
+
 def fetch_events(client: Client, fetch_limit: int, first_fetch: str, last_run: dict) -> list:
     """
     Format the payload based on event_type i.e. (activities or aggregates) and the fetch the
@@ -123,24 +137,23 @@ def fetch_events(client: Client, fetch_limit: int, first_fetch: str, last_run: d
     Perform deduplication of fetched response by comparing with the previous fetch
     """
 
-    '''
-    Number of miliseconds to push back for refetching
-    in case no duplicates are found.
-    '''
+    # Grace period (ms) to refetch earlier events when overlap is not detected
     refetch_grace_period_ms = 500
 
+    # Get the last execution data
+    # time - latest processed event timestamp
+    # ids - event IDs that share the latest timestamp
+    # cursor - pagination token (if more pages remained)
     last_fetch = last_run.get('time')
     last_event_ids = last_run.get('ids', [])
     cursor = last_run.get('cursor', None)
 
+    # Initialize first fetch timestamp when integration runs for the first time
     if not last_fetch:
         first_fetch_dt = dateparser.parse(first_fetch, settings={'RELATIVE_BASE': datetime.now(timezone.utc)})
         last_fetch = first_fetch_dt.strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3] + "Z"
 
-    '''
-    flag to denote if paginated events is fetched or
-    using the from timestamp
-    '''
+    # Flag to track whether continuing a paginated fetch cycle of events
     fetch_next_page = False
 
     params = {
@@ -148,9 +161,14 @@ def fetch_events(client: Client, fetch_limit: int, first_fetch: str, last_run: d
         "sortOrder": "asc"
     }
 
+    # Determine fetch mode:
+    # Use timestamp when (if block):
+    #   - If it's first integration fetch
+    #   - Or when pagination completed in last fetch cycle
+    # Use cursor when (else block): 
+    #   - continuing fetch of paginated results
     if cursor is None:
         # Update time filter in API payload
-        # current_time = current_utc_milliseconds()
         from_timestamp = iso_to_milliseconds(last_fetch)
         params["from"] = from_timestamp
     else:
@@ -158,37 +176,40 @@ def fetch_events(client: Client, fetch_limit: int, first_fetch: str, last_run: d
         # set flag to True when using cursor to fetch next page events
         fetch_next_page = True
 
-    '''
-    Send api call with api parameters to fetch the events
-    '''
+    # Fetch events from API (returns events and pagination metadata)
+    # Extract the pagination cursor from meta data
     events, meta_data = client.get_events(params)
     next_cursor = meta_data.get("next")
 
-    '''
-    if next page is fetched, then deduplication check is not required,
-    else check for duplicate events
-    '''
+    # Apply overlap check and refetch only when:
+    #   - Not the integration's first fetch (if last_events_ids are present)
+    #   - Events are present in the current fetch
+    #   - And continuing paginated events are not fetched in current cycle
     if not fetch_next_page:
-        '''
-        Check if refetch is required based on following parameters:
-        - If events ids are present from last fetch
-        - If no overlapping events are found
-        '''
-        if events and last_event_ids and not overlap_exists(events, last_event_ids):
+
+        # If no overlapping event IDs are found, refetch with grace window
+        # Refetch events when:
+        #   - last_events_ids are present
+        #   - events are present in the current fetch
+        #   - no overlapping events are found (by matching if last_event_ids are present in current fetched events)
+        if should_refetch_events(events, last_event_ids):
             # refetch logic
             refetch_time = from_timestamp - refetch_grace_period_ms
             params["from"] = refetch_time
 
             demisto.updateModuleHealth(f"No overlapping event id found. Refetching with grace time(ms): {refetch_time}")
-            demisto.debug(f"No overlapping event_id found in {event_type}. Refetching with grace time(ms): {refetch_time}")
-            # fetch the events with reduced timestamp
+            demisto.debug(f"No overlapping event id found. Refetching with grace time(ms): {refetch_time}")
+            
+            # Re-fetch events using adjusted timestamp and get the next cursor from meta data if present
             events, meta_data = client.get_events(params)
             next_cursor = meta_data.get("cursor")
 
+            # Log potential data loss if overlap still does not exist after refetch
             if not overlap_exists(events, last_event_ids):
                 demisto.updateModuleHealth(f"Still no overlapping event_id found after refetch with time={refetch_time}. Possible API data loss.")
                 demisto.debug(f"Still no overlapping event_id found after refetch with time={refetch_time}. Possible API data loss.")
 
+        # Remove the events with matching events ids as in the last_event_ids
         events = deduplicate_events(events, last_event_ids, last_fetch)
 
     return events, next_cursor
